aee32593adfd6dc866c0188e1e5b92fb2e042a8f
xingcheng412412 <148077919+xingcheng412412@users.noreply.github.com>
2024-03-06 20:13:17 +0800

    Add files via upload
    雷达文件

---
+++ 5（复件）/build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/sunwei/5（复件）/main5.cpp" "/home/sunwei/5（复件）/build/CMakeFiles/main.dir/main5.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "IE_THREAD=IE_THREAD_TBB"
+  "OV_THREAD=OV_THREAD_TBB"
+  "TBB_PREVIEW_WAITING_FOR_WORKERS=1"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/cuda/include"
+  "/usr/local/zed/include"
+  "/opt/intel/openvino_2023/runtime/include"
+  "/usr/include/opencv4"
+  "/opt/intel/openvino_2023.3.0/runtime/include"
+  "/opt/intel/openvino_2023.3.0/runtime/include/ie"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
